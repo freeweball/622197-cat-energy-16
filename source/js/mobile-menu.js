c12c2c48d934c20947d3b6d5ec83ee28333cda63
Aleksandr <asavostin1@gmail.com>
2019-05-22 23:42:27 +0500

    Добавляет JS

---
+++ source/js/mobile-menu.js
@@ -0,0 +1,20 @@
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.page-header__toggle');
+
+navMain.classList.remove('main-nav--nojs');
+
+if (navToggle.classList.contains('page-header__toggle--close')) {
+  navToggle.classList.remove('page-header__toggle--close');
+  navToggle.classList.add('page-header__toggle--open');
+}
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.toggle('main-nav--closed');
+    navToggle.classList.toggle('page-header__toggle--close');
+
+  } else {
+    navMain.classList.toggle('main-nav--closed');
+    navToggle.classList.toggle('page-header__toggle--close');
+  }
+});
