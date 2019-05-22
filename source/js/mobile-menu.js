a8718be3be3516c51bd164fb166ff3d92934c4a7
Aleksandr <asavostin1@gmail.com>
2019-05-23 01:24:05 +0500

    Доработки

--- source/js/mobile-menu.js
+++ source/js/mobile-menu.js
@@ -3,18 +3,21 @@ var navToggle = document.querySelector('.page-header__toggle');
 
 navMain.classList.remove('main-nav--nojs');
 
-if (navToggle.classList.contains('page-header__toggle--close')) {
+if (navMain.classList.contains('main-nav--open')) {
+  navMain.classList.remove('main-nav--open');
+  navMain.classList.add('main-nav--closed');
   navToggle.classList.remove('page-header__toggle--close');
   navToggle.classList.add('page-header__toggle--open');
 }
 
 navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--open')) {
+  if (navMain.classList.contains('main-nav--closed')) {
     navMain.classList.toggle('main-nav--closed');
-    navToggle.classList.toggle('page-header__toggle--close');
-
+    navToggle.classList.toggle('page-header__toggle--open');
+    navToggle.classList.add('page-header__toggle--close');
   } else {
     navMain.classList.toggle('main-nav--closed');
     navToggle.classList.toggle('page-header__toggle--close');
+    navToggle.classList.add('page-header__toggle--open');
   }
 });
